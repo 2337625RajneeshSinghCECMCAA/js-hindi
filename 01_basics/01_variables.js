da1f9ed3f9c234731d4f3b6baf233310f33ba899
Rajneesh123 <142533255+2337625RajneeshSinghCECMCAA@users.noreply.github.com>
2025-08-04 08:22:08 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 4567
+let accountEmail = "rajneesh@google.com"
+let accountPassword ="12345"
+accountCity ="Chamba"
+
+
+accountEmail ="raj@youtube.com"
+accountPassword ="23411"
+accountCity ="palampur"
+
+console.table([accountId, accountEmail, accountPassword,accountCity ])
+
